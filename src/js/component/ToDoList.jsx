750dd0bd894efcffde9abd49fb5affea902e7a96
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-04-24 19:29:53 +0000

    final

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -10,40 +10,41 @@ export const ToDoList = () => {
   };
 
   const enterKey = (event) => {
-    event.key === "Enter" ? (setToDoTask(toDoTask.concat([task])), setTask("")) : null;
+    event.key === "Enter" && task.trim() !== "" ? (setToDoTask(toDoTask.concat([task])), setTask("")) : null;
   }; 
 
   const deleteTask = (index) => {
     
-    const finishedTask = [toDoTask];
-    finishedTask.splice(index, 1);
-    setToDoTask(finishedTask);
-  };
-
-  
+    
+    setToDoTask(toDoTask.filter((element) => element !== index))
+    
+  }
 
   return (
-    <div class="container-fluid m-0 d-flex justify-content-center text-center">
+    <div className="container-fluid m-0 d-flex justify-content-center text-center">
     <div>    
       <h1> To-do's</h1>
-      <ul class="list-group list-group-flush" style="max-width: 500px;">
-        <li class="list-group-item d-flex align-items-center">
+      <ul className="list-group list-group-flush" style={{ maxWidth: '500px' }}>
+        <li className="list-group-item d-flex align-items-center">
           <input type="text" placeholder="Tasks to be done" value={task}
                  onChange={handleTask} onKeyPress={enterKey}
-                 class="form-control flex-grow-1" />
+                 className="form-control flex-grow-1" />
         </li>
             {toDoTask.map((item, index) => (
-            <li key={index} class="list-group-item d-flex align-items-center">
-            <span class="flex-grow-1 text-start">{item}</span>
-            <a href="#"><i class="fa-solid fa-square-minus text-secondary hideTask viewTask" onClick={deleteTask}></i></a>
+            <li key={index} className="list-group-item d-flex align-items-center">
+            <span className="flex-grow-1 text-start">{item}</span>
+            <span onClick={() => deleteTask(item)}>
+  <i className="fa-solid fa-square-minus text-secondary showButton"></i>
+</span>
           </li>
         ))}
       </ul>
-      <span class="d-flex text-start mx-3">
+      <span className="d-flex text-start mx-3">
         {toDoTask.length === 0 ? "You don't have any pending task" : toDoTask.length + " Pending tasks" }
       </span>
     </div>
-  </div>
+
+    </div>
   
   );
 };
