1ee859b816eff0a5d5ee59927d1a369f9a993bd3
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-04-25 12:01:49 +0000

    Otro mas

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -14,37 +14,34 @@ export const ToDoList = () => {
   }; 
 
   const deleteTask = (index) => {
-    
-    
-    setToDoTask(toDoTask.filter((element) => element !== index))
-    
-  }
+    setToDoTask(toDoTask.filter((element) => element !== index));
+  };
 
   return (
-    <div className="container-fluid m-0 d-flex justify-content-center text-center">
-    <div>    
-      <h1> To-do's</h1>
-      <ul className="list-group list-group-flush" style={{ maxWidth: '500px' }}>
-        <li className="list-group-item d-flex align-items-center">
-          <input type="text" placeholder="Tasks to be done" value={task}
-                 onChange={handleTask} onKeyPress={enterKey}
-                 className="form-control flex-grow-1" />
-        </li>
-            {toDoTask.map((item, index) => (
-            <li key={index} className="list-group-item d-flex align-items-center">
-            <span className="flex-grow-1 text-start">{item}</span>
-            <span onClick={() => deleteTask(item)}>
-  <i className="fa-solid fa-square-minus text-secondary showButton"></i>
-</span>
+    <div className="container-fluid m-auto d-flex justify-content-center text-center bg-light rounded-4" style={{maxWidth: "600px", height: "800px"}}>
+      <div id="listTodo" className="bg-light">    
+        <h1> To-do's</h1>
+        <ul className="list-group list-group-flush bg-light">
+          <li className="list-group-item d-flex align-items-center bg-light text-center">
+            <input type="text" placeholder="Tasks to be done" value={task}
+                   onChange={handleTask} onKeyPress={enterKey}
+                   className="form-control mx-auto inputWidth" />
           </li>
-        ))}
-      </ul>
-      <span className="d-flex text-start mx-3">
-        {toDoTask.length === 0 ? "You don't have any pending task" : toDoTask.length + " Pending tasks" }
-      </span>
+          {toDoTask.map((item, index) => (
+            <li key={index} className="list-group-item d-flex text-start bg-light align-items-center hidden-icon">
+              <label className=" text-start">{item}</label>
+              <span onClick={() => deleteTask(item)}>
+                <i className="fa-solid fa-square-minus text-secondary showButton"></i>
+              </span>
+            </li>
+          ))}
+        </ul>
+        <div className="align-items-end">
+        <span id="tasksCount" className="d-block text-start mx-3">
+          {toDoTask.length === 0 ? "You don't have any pending task" : toDoTask.length + " Pending tasks" }
+        </span>
+        </div>
+      </div>
     </div>
-
-    </div>
-  
   );
 };
