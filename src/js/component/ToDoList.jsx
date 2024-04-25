7f0e125eda20b8fc9977a9a9abe30b6bfcb70d59
Robert <155775143+Roberttovarv@users.noreply.github.com>
2024-04-25 19:17:56 +0000

    i final
    Signed-off-by: Robert <155775143+Roberttovarv@users.noreply.github.com>

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "../../styles/index.css";
+import seam1 from "../../img/seam1.jpg";
+import seam2 from "../../img/seam2.jpg";
+
 
 export const ToDoList = () => {
   const [task, setTask] = useState("");
@@ -17,11 +20,11 @@ export const ToDoList = () => {
     setToDoTask(toDoTask.filter((element) => element !== index));
   };
 
-  return (
-    <div className="my-3 container-fluid m-auto d-flex justify-content-center text-center bg-light rounded-4 position-relative" style={{ maxWidth: "600px", height: "100vh" }}>
-  <div id="listTodo" className="my-3 bg-light">
-    <h1> To-do's</h1>
-    <ul className="list-group list-group-flush bg-light border-bottom-1">
+  return ( <div className="">
+    <div className=" my-3 container-fluid m-auto d-flex flex-column justify-content-center text-center bg-light rounded-4 position-relative" style={{ maxWidth: "600px", width: "50%", height: "93vh", minWidth: "300px",}}>
+    <div><h1> To-do's</h1></div>
+  <div id="listTodo" className="my-2 bg-light" style={{ overflowY: "auto", height: "85%" }}>
+    <ul className="list-group list-group-flush bg-light">
       <li id="inputLi" className="list-group-item mt-3 d-flex align-items-center bg-light text-center">
         <input
           type="text"
@@ -29,24 +32,27 @@ export const ToDoList = () => {
           value={task}
           onChange={handleTask}
           onKeyPress={enterKey}
-          className="form-control mx-auto inputWidth"
+          className="form-control mx-auto inputWidth rounded-4"
         />
       </li>
       {toDoTask.map((item, index) => (
-        <li key={index} className="list-group-item shadow g-3 d-flex text-start bg-light align-items-center hidden-icon">
-          <label className="text-start">{item}{item.length > 70 ? <br /> : ""}</label>
-          <span onClick={() => deleteTask(item)}>
-            <i className="fa-solid fa-square-minus text-secondary"></i>
-          </span>
-        </li>
+  <li key={index} className="list-group-item my-2 shadow-sm g-3 rounded-4 d-flex text-start bg-light align-items-center ">
+    <div className="d-flex justify-content-between w-100 hidden-icon">
+      <label className="text-start">{item}{item.length > 70 ? <br /> : ""}</label>
+      <span onClick={() => deleteTask(item)}>
+        <i className="fa-solid fa-square-minus text-secondary "></i>
+      </span>
+    </div>
+  </li>
       ))}
     </ul>
-    <div className="position-absolute bottom-0 start-0 mx-4 mb-3">
-      <h4 id="tasksCount" className="d-block text-start">
-        {toDoTask.length === 0 ? "You don't have any pending task" : toDoTask.length + " Pending tasks"}
-      </h4>
-    </div>
   </div>
+    <div className="position-absolute bottom-0 start-0 mx-4 mt-5 pt-5 mb-3">
+      <h5 id="tasksCount" className="d-block text-start">
+        {toDoTask.length === 0 ? "You don't have any pending task" : toDoTask.length + " Pending tasks"}
+      </h5>
+    </div>
+</div>
 </div>
 
   );
